1d67e044cc585e4baf815cab490ec24420f144cf
LittleAnjel <kelveag2017@gmail.com>
2025-09-01 12:20:45 -0300

    Update Program.cs

--- WebApiTarefas/Program.cs
+++ WebApiTarefas/Program.cs
@@ -17,16 +17,10 @@ builder.Services.AddScoped<ITarefaService, TarefaServico>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen();
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    //app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
 app.Run();
